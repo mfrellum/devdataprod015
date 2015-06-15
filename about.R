8c38af80bd5e7dc82e94ee849736dbc08e6656ef
mfrellum@gmail.com <mfrellum@gmail.com>
2015-06-15 15:31:34 +0200

    App ready but details are missing from help.R and about.R

---
+++ about.R
@@ -0,0 +1,6 @@
+div(
+    h1("About"),
+    h2("Here you will find information About the app"),
+    p("http://sebastianraschka.com/Articles/2014_intro_supervised_learning.html"),
+    p("https://en.wikipedia.org/wiki/Iris_flower_data_set")
+)
---
+++ help.R
@@ -0,0 +1,5 @@
+div(
+    h1("Help"),
+    h2("Here you will find help."),
+    img(src='iris_petal_sepal.png')
+)
